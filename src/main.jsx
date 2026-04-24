1ee9785a138d1394c1e7173156c35cbf37b8136e
niraj <nirajssatam@gmail.com>
2026-04-25 04:24:08 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,7 @@ import App from './App.jsx'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
